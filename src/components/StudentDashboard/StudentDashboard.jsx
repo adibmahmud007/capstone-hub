b7bda7ad995388b81e9ba6e13315dfd95a5e3852
adibmahmud007 <adibmahmud307@gmail.com>
2025-02-11 23:55:17 +0600

    Student Dashboard created

---
+++ src/components/StudentDashboard/StudentDashboard.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const StudentDashboard = () => {
+    return (
+        <div>
+            hello from student.
+        </div>
+    );
+};
+
+export default StudentDashboard;
