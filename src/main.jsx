de3018eca1abde4ff7c1ea5db228248e9e858507
adibmahmud007 <adibmahmud307@gmail.com>
2025-04-30 23:15:52 +0600

    toast time reduced to 3 sec

--- src/main.jsx
+++ src/main.jsx
@@ -53,7 +53,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ToastContainer
       position="top-center"
-      autoClose={5000}
+      autoClose={3000}
       hideProgressBar={false}
       newestOnTop={false}
       closeOnClick={false}
